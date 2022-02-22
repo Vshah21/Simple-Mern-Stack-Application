30a711447c57eeb1dc312846b52641790c41a0e3
vishal shah <shahvishal774@gmail.com>
2022-02-22 11:10:41 +0000

    second commit

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 5000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '3000') +
-    '/mernproject'
+    mongoUri: process.env.MONGODB_URI
    }
 module.exports = {
     config
